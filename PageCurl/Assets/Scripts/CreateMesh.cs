2a26a2a2903eb9de57b515040695ba2172c5eac9
Macx <Macx@drkdeMacBook-Pro.local>
2017-02-21 17:50:41 +0800

    a page curl demo used Implementing iBooks page curling using a conical deformation algorithm Control the A B and theta to different curl

---
+++ PageCurl/Assets/Scripts/CreateMesh.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMesh : MonoBehaviour {
+
+	private GameObject mMesh;
+	private Material mMaterial;
+
+	/*   Mesh
+	 *     with  length
+	 *     segment
+	 *     height
+	 *
+	 */
+
+	private Vector2 size;
+//	private float height = 0;
+	private Vector2 segment;
+
+
+	/*  vertex
+	 *  uv
+	 *  triangles
+	 */
+
+	private Vector3[] vertexes;
+	private int[] triangles;
+
+
+	/*
+	 * compute vertexes
+	*/
+
+	private void computeVertexes()
+	{
+		int sum = Mathf.FloorToInt ((segment.x + 1)*(segment.y + 1));
+		float w = size.x / segment.x;
+		float h = size.y / segment.y;
+
+		GetTriangles ();
+
+		int index = 0;
+		vertexes = new Vector3[sum];
+		for (int i = 0; i < segment.y+1; i++) 
+		{
+			for (int j = 0; j< segment.x + 1; j++)
+			{
+//				float tempHeight = 0;
+				vertexes[index] = new Vector3(j*w,0,i*h);
+				index++;
+			}
+		}
+
+	}
+
+	/*
+	 * getTriangles
+	 */
+
+	private int[] GetTriangles()
+	{
+		int sum = Mathf.FloorToInt (segment.x * segment.y * 6);//三角形顶点总数：假设是1*1的网格，会有2个顶点复用，因此是6个顶点。假设是2*2的网格，则是4个1*1的网格，即4*6即2*2*6！
+		triangles = new int[sum];
+		uint index = 0;
+
+		for (int i = 0; i < segment.y; i++) 
+		{
+			for(int j = 0; j< segment.x; j++)
+			{
+				int role = Mathf.FloorToInt(segment.x)+1;
+				int self = j + (i*role);
+				int next = j + ((i+1)*role);
+
+
+				//first triangle
+				triangles[index] = self;
+				triangles[index + 1] = next + 1;
+				triangles[index + 2] = self + 1;
+
+				//second triangle
+				triangles[index + 3] =  self;
+				triangles[index + 4] = next;
+				triangles[index + 5] = next + 1;
+				index += 6;
+
+			}
+		}
+
+		return triangles;
+	}
+
+	private void DrawMesh()
+	{
+		Mesh mesh = mMesh.AddComponent<MeshFilter> ().mesh;
+		mMesh.AddComponent<MeshRenderer> ();
+
+		mMaterial = new Material(Shader.Find("custom/BumpSpec_Twoside"));
+
+		mMesh.GetComponent<Renderer> ().material = mMaterial;
+
+		mesh.Clear ();
+		mesh.vertices = vertexes;
+
+		mesh.triangles = triangles;
+
+		mesh.RecalculateNormals ();
+		mesh.RecalculateBounds ();
+	}
+
+	private void createMesh(float width, float height,uint segmentX, uint segmentY,int min,int max)
+	{
+		size = new Vector2 (width, height);
+		segment = new Vector2 (segmentX, segmentY);
+
+		if (mMesh != null)
+		{
+			Destroy(mMesh);
+		}
+
+		mMesh = new GameObject ();
+		mMesh.name = "CreateMesh";
+
+		computeVertexes ();
+		DrawMesh ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		createMesh (10, 10, 3, 3, -10, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ PageCurl/Assets/Scripts/PageCurl.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PageCurl : MonoBehaviour {
+
+	[Range(0, 1.6f)]
+	public float theta;
+
+	public Vector2 A;
+
+	public float B = 0;
+
+	public float rho;
+
+	private Mesh mesh;
+	private Vector3[] vertices;
+	void Start()
+	{
+		mesh = this.GetComponent<MeshFilter> ().mesh;
+		vertices = new Vector3[mesh.vertices.Length];
+		for (int i = 0; i < mesh.vertices.Length; i++) 
+		{
+			vertices[i] = mesh.vertices[i];
+		}
+//		Deform ();
+	}
+
+	private void Deform()
+	{
+		Vector3 vi = new Vector3();
+		Vector3 v1 = new Vector3();
+		Vector3 vo = new Vector3();
+
+		Vector3[] newVertices = new Vector3[mesh.vertices.Length];
+;
+		float R, r, beta;
+		for (int i = 0; i < mesh.vertices.Length; i++) 
+		{
+			vi = vertices[i];
+
+			/*
+			 * 
+			 * 
+			 * Version   A
+			 * 
+			 * 
+			 */
+//			R = Mathf.Sqrt(vi.x * vi.x + Mathf.Pow(vi.y - A.y ,2));
+//
+//			r = R * Mathf.Sin(theta);
+//
+//			beta = Mathf.Asin(vi.x / R) / Mathf.Sin(theta);
+//
+//
+//			v1.x = r * Mathf.Sin(beta);
+//			v1.y = R + A.y - r* (1 - Mathf.Cos(beta))*Mathf.Sin(theta);
+//			v1.z = r * (1-Mathf.Cos(beta)) * Mathf.Cos(theta);
+
+
+			/*
+			 * 
+			 * 
+			 * VersionB
+			 * 
+			 */
+
+			R = Mathf.Sqrt(Mathf.Pow(vi.x - B,2) + Mathf.Pow(vi.y - A.y ,2));
+			r = R * Mathf.Sin(theta);
+//			beta = Mathf.Asin(vi.x / R) / Mathf.Sin(theta);
+
+			if(vi.x < B)
+			{
+				beta = 0;
+			}else
+			{
+				beta = Mathf.Asin((vi.x-B) / R) / Mathf.Sin(theta);
+			}
+
+			if(vi.x < B)
+			{
+				v1.x = vi.x;
+			}else{
+				v1.x = B + r * Mathf.Sin(beta);
+			}
+
+			if(vi.x < B)
+			{
+				v1.y = vi.y;
+			}else{
+				v1.y = R + A.y - r* (1 - Mathf.Cos(beta))*Mathf.Sin(theta);
+			}
+
+			v1.z = r * (1-Mathf.Cos(beta)) * Mathf.Cos(theta);
+
+			newVertices[i].x = v1.x;
+			newVertices[i].y = v1.y;
+			newVertices[i].z = v1.z;
+
+			vo.x = (v1.x * Mathf.Cos(rho) - v1.z * Mathf.Sin(rho));
+			vo.y = v1.y;
+			vo.z = (v1.x * Mathf.Sin(rho) + v1.z * Mathf.Cos(rho));
+		}
+
+		mesh.vertices = newVertices;
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+	}
+
+	private void OnDrawGizmos () {
+		Gizmos.color = Color.black;
+		for (int i = 0; i < mesh.vertices.Length; i++) {
+
+			if (mesh.vertices[i] == null) {
+				return;
+			}
+
+			Gizmos.DrawSphere(mesh.vertices[i], 0.1f);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Deform ();
+	}
+
+}
+
+
+
+//http://blog.flirble.org/2010/10/08/the-anatomy-of-a-page-curl/
+//http://wdnuon.blogspot.jp/2010/05/implementing-ibooks-page-curling-using.html
+//http://www2.parc.com/istl/groups/uir/publications/items/UIR-2004-10-Hong-DeformingPages.pdf
